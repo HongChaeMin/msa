9eb4bd748b42271e7ace0142c33cca2e398d67fb
홍채민 <coals0329@cocen.com>
2022-04-19 12:29:39 +0900

    feat(gateway): add LoggingFilter

--- gateway-service/src/main/java/com/example/gatewayservice/filter/CustomFilter.java
+++ gateway-service/src/main/java/com/example/gatewayservice/filter/CustomFilter.java
@@ -24,7 +24,6 @@ public class CustomFilter extends AbstractGatewayFilterFactory<CustomFilter.Conf
     public GatewayFilter apply(Config config) {
         // Custom Pre Filter. Suppose we can extract JWT and perform Authentication
 
-        // ex1)
         //filter에서 하고 싶은 내용을 재정의
         //pre filter 동작
         return ((exchange, chain) -> {
@@ -40,20 +39,6 @@ public class CustomFilter extends AbstractGatewayFilterFactory<CustomFilter.Conf
                 log.info("Custom POST filter : response id -> {}", response.getStatusCode());
             }));
         });
-
-        // ex2)
-        // 이걸로 설정하면 글로벌 필터가 더 늦게 작동함
-        /*return new OrderedGatewayFilter((exchange, chain)->{    // WebFlux를 활용하여 비동기 처리에서 request와 response를 가져올 수 있다.
-            ServerHttpRequest request = exchange.getRequest();
-            ServerHttpResponse response = exchange.getResponse();
-
-            log.info("Custom PRE filter : request id -> {}", request.getId());
-
-            // post filter 동작
-            return chain.filter(exchange).then(Mono.fromRunnable(()->{  // 스프링5에서 지원하는 기능으로 비동기로 값을 전달할때 사용되는 객체
-                log.info("Custom POST filter : response id -> {}", response.getStatusCode());
-            }));
-        }, Ordered.HIGHEST_PRECEDENCE); //필터의 우선순위 */
     }
 
     public static class Config {

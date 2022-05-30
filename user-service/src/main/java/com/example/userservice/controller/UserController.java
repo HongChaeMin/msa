5fe42002b4f9579d097e8f12506fdeeada8a4a0f
홍채민 <coals0329@cocen.com>
2022-05-30 11:38:42 +0900

    users(microservice): JPA 1

--- user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -1,9 +1,16 @@
 package com.example.userservice.controller;
 
+import com.example.userservice.dto.UserDto;
+import com.example.userservice.service.UserService;
 import com.example.userservice.vo.Greeting;
+import com.example.userservice.vo.RequestUser;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,8 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final Environment env;
-
     private final Greeting greeting;
+    private final UserService userService;
 
     @GetMapping("/heath-check")
     public String status() {
@@ -23,6 +30,15 @@ public class UserController {
     public String wellcome() {
         // return env.getProperty("greeting.message");
         return greeting.getMessage();
+    }
+
+    @PostMapping("/users")
+    public String createUser(@RequestBody RequestUser user) {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        UserDto userDto = modelMapper.map(user, UserDto.class);
+        userService.createUser(userDto);
+        return "Create user method is called";
     }
 
 }

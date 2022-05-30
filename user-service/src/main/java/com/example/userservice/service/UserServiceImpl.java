766043e7afeba1d3f1a102604558898a79093b9d
홍채민 <coals0329@cocen.com>
2022-05-30 11:38:42 +0900

    users(microservice): JPA 1

---
+++ user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -0,0 +1,33 @@
+package com.example.userservice.service;
+
+import com.example.userservice.dto.UserDto;
+import com.example.userservice.repository.UserEntity;
+import com.example.userservice.repository.UserRepository;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.spi.MatchingStrategy;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDto createUser(UserDto userDto) {
+        userDto.setUserId(UUID.randomUUID().toString().substring(0, 6));
+
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        UserEntity userEntity = modelMapper.map(userDto, UserEntity.class);
+        userEntity.setEncryptedPwd("encrypted_password");
+
+        userRepository.save(userEntity);
+
+        return userDto;
+    }
+
+}
